e8a3de380f3161d8c8f220baab10f624511d329b
gc313 <gc313@foxmail.com>
2023-04-21 22:51:05 +0800

    v0.0.2

---
+++ app.py
@@ -0,0 +1,65 @@
+import building as bd
+import streamlit as st
+import configparser
+
+#读取配置文件
+file = 'setting.ini'
+config = configparser.ConfigParser()
+config.read(file, encoding = 'utf-8')
+num_items = dict(config.items('Qvzhi'))
+
+
+st.title = '口算生成器'
+
+with st.sidebar:
+    
+    totalNum = st.slider('算式数量', min_value=1, max_value=100, value=int(num_items['ss_number']), step=1, format=None)
+    
+    jiaSet = st.checkbox('加法')
+    jia_col1, jia_col2 = st.columns(2)
+    with jia_col1:
+        jiaMin = st.number_input('运算项最小取值', value= int(num_items['jia_min']), min_value=0, step=1, format='%d')
+    with jia_col2:
+        jiaMax = st.number_input('运算项最大取值', value= int(num_items['jia_max']), min_value=0, step=1, format='%d')
+
+
+
+    jianSet = st.checkbox('减法')
+    jian_col1, jian_col2 = st.columns(2)
+    with jian_col1:
+        jianMin = st.number_input('运算项最小取值', value= int(num_items['jian_min']), min_value=0, step=1, format='%d')
+    with jian_col2:
+        jianMax = st.number_input('运算项最大取值', value= int(num_items['jian_max']), min_value=0, step=1, format='%d')
+
+
+
+    chengSet = st.checkbox('乘法')
+    cheng_col1, cheng_col2 = st.columns(2)
+    with cheng_col1:
+        chengMin = st.number_input('运算项最小取值', value= int(num_items['cheng_min']), min_value=0, step=1, format='%d')
+    with cheng_col2:
+        chengMax = st.number_input('运算项最大取值', value= int(num_items['cheng_max']), min_value=0, step=1, format='%d')
+
+
+
+    chuSet = st.checkbox('除法')
+    chu_col1, chu_col2 = st.columns(2)
+    with chu_col1:
+        beichuMin = st.number_input('被除数最小取值', value= int(num_items['beichu_min']), min_value=0, step=1, format='%d')
+    with chu_col2:
+        beichuMax = st.number_input('被除数最大取值', value= int(num_items['beichu_max']), min_value=0, step=1, format='%d')
+
+    chu_col3, chu_col4 = st.columns(2)
+    with chu_col3:
+        chuMin = st.number_input('除数最小取值', value= int(num_items['chu_min']), min_value=0, step=1, format='%d')
+    with chu_col4:
+        chuMax = st.number_input('除数最大取值', value= int(num_items['chu_max']), min_value=0, step=1, format='%d')
+
+    chuOption = st.selectbox('是否生成带余数算式？', ('不要生成带余数算式', '总是生成带余数算式', '都可以'))
+
+
+    b_col1, b_col2 = st.columns(2)
+    with b_col1:
+        saveCfg = st.button('保存设置')
+    with b_col2:
+        create = st.button('生成算式')
--- building.py
+++ building.py
@@ -1,6 +1,31 @@
-import setting as s
+import configparser
 import random
 
+#读取配置文件
+file = 'setting.ini'
+config = configparser.ConfigParser()
+config.read(file, encoding = 'utf-8')
+items = dict(config.items('Qvzhi'))
+
+jiaMin = int(items['jia_min'])      #加法算式取值最小数
+jiaMax = int(items['jia_max'])      #加法算式取值最大数
+jiaN = 8         #加法算式数量
+
+jianMin = int(items['jian_min'])      #减法算式取值最小数
+jianMax = int(items['jian_max'])     #减法算式取值最大数
+jianN = 8        #减法算式数量
+
+chengMin = int(items['cheng_min'])     #乘法算式取值最小数
+chengMax = int(items['cheng_max'])     #乘法算式取值最大数
+chengN = 8       #乘法算式数量
+
+beichuMin = int(items['beichu_min'])   #被除数取值最小数
+beichuMax = int(items['beichu_max'])   #被除数取值最大数
+chuMin = int(items['chu_min'])       #除数取值最小数
+chuMax = int(items['chu_max'])       #除数取值最大数
+chuN = 8        #除法算式数量
+yuShu = int(items['yu_shu'])        #是否有余数 0:没有   1：都有   2：随机
+deShuMax = int(items['deshu_max'])     #除法最大得数
 
 def Jia(Min, Max, N):
 	jiaList = []
@@ -8,9 +33,9 @@ def Jia(Min, Max, N):
 		suanshi = str(random.randint(Min, Max))+" + "+str(random.randint(Min, Max))+" = "
 		jiaList.append(suanshi)
 		i += i + 1
-		return jiaList
+	return jiaList
 
-#print(Jia(jiaMin, jiaMax, jiaN)) #调试"
+#print(Jia(jiaMin, jiaMax, jiaN)) #调试
 
 def Cheng(Min, Max, N):
 	chengList = []
@@ -18,9 +43,9 @@ def Cheng(Min, Max, N):
 		suanshi = str(random.randint(Min, Max))+" × "+str(random.randint(Min, Max))+" = "
 		chengList.append(suanshi)
 		i += i + 1
-		return chengList
+	return chengList
 
-#print(Cheng(chengMin, chengMax, chengN)) #调试"
+#print(Cheng(chengMin, chengMax, chengN)) #调试
 
 
 def Jian(Min, Max, N):
@@ -30,28 +55,28 @@ def Jian(Min, Max, N):
 		suanshi = str(max(numbers))+" - "+str(min(numbers))+" = "
 		jianList.append(suanshi)
 		i += i + 1
-		return jianList
+	return jianList
 
-#print(Jian(jianMin, jianMax, jianN)) #调试"
+#print(Jian(jianMin, jianMax, jianN)) #调试
 
 
-def Chu2(bcMin, bcMax, cMin, cMax, yuShu, deShu, N):
+def Chu(bcMin, bcMax, cMin, cMax, yuShu, deShu, N):
 	chuList = []
 	for i in range(N):
 		numbers = [random.randint(bcMin, bcMax), random.randint(cMin, cMax)]
-		bcS, cS = max(numbers), min(numbers) #被除数和除数
+	
 		if yuShu == 0:
-			while bcS % cS != 0 or bcS / cS >= deShu + 1:   #当得数不为整数并且得数大于所选得数范围时重新选数
+			while max(numbers) % min(numbers) != 0 or max(numbers) / min(numbers) >= deShu + 1:   #当得数不为整数并且得数大于所选得数范围时重新选数
 				numbers = [random.randint(bcMin, bcMax), random.randint(cMin, cMax)]
 		elif yuShu == 1:
-			while bcS % cS == 0 or bcS / cS >= deShu + 1:  #当得数为整数并且得数大于所选得数范围时重新选数
+			while max(numbers) % min(numbers) == 0 or max(numbers) / min(numbers) >= deShu + 1:  #当得数为整数并且得数大于所选得数范围时重新选数
 				numbers = [random.randint(bcMin, bcMax), random.randint(cMin, cMax)]
 		elif yuShu == 2:
-			while bcS / cS >= deShu + 1:  #当得数大于所选得数范围时重新选数
+			while max(numbers) / min(numbers) >= deShu + 1:  #当得数大于所选得数范围时重新选数
 				numbers = [random.randint(bcMin, bcMax), random.randint(cMin, cMax)]
-		suanshi = str(bcS)+" ÷ "+str(cS)+" = "
+		suanshi = str(max(numbers))+" ÷ "+str(min(numbers))+" = "
 		chuList.append(suanshi)
 		i += i + 1
-		return chuList,
+	return chuList,
 
-#print(Chu(beichuMin, beichuMax, chuMin, chuMax, yuShu, deShuMax, chuN)) #调试"
+#print(Chu(beichuMin, beichuMax, chuMin, chuMax, yuShu, deShuMax, chuN)) #调试
