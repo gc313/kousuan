ede3a01d06dec937ac12647fed0dd68932c6fe38
gc313 <gc313@foxmail.com>
2023-04-25 17:42:42 +0800

    v1.0.0

--- building.py
+++ building.py
@@ -1,5 +1,5 @@
 '''
-Copyright (C) 2023
+Copyright (C) 2023 Email:gc313@foxmail.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
