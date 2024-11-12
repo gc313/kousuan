f3bb4d25fd3a39abeb70e6013e60543046584fce
Offline <gc313@foxmail.com>
2024-11-12 11:08:54 +0800

    refactor(app): 重构了应用的代码结构和功能
    - 优化了配置文件读取逻辑
    - 重构了表单创建和参数处理逻辑
    - 改进了 HTML 生成函数
    - 优化了版本信息获取方法
    - 重构了算式生成逻辑，提高了代码可读性和维护性
    - 调整了部分默认配置值

--- app.py
+++ app.py
@@ -1,224 +1,180 @@
 '''
-Copyright (C) 2023 Email:gc313@foxmail.com
+Copyright (C) 2024 Email:gc313@foxmail.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or any 
+the Free Software Foundation, either version 3 of the License, or any
 later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 '''
+import building as bd  # 导入 building 模块
+import streamlit as st  # 导入 Streamlit 库
+import configparser  # 导入配置文件解析库
+import math  # 导入数学库
+import requests  # 导入 HTTP 请求库
 
-import building as bd
-import streamlit as st
-import configparser
-import streamlit.components.v1 as components
-import math
-import requests
+st.set_page_config(page_title="小学生数学练习题生成器", layout="wide", page_icon="💯")  # 设置 Streamlit 页面配置
 
+# 读取配置文件
+file = 'setting.ini'
+config = configparser.ConfigParser()
+config.read(file, encoding='utf-8')  # 读取配置文件
 
+# 默认值
+DEFAULT_MIN_VALUE = 0
+DEFAULT_MAX_VALUE = 99
 
-st.set_page_config(page_title="小学生数学练习题生成器",layout="wide",page_icon="💯")
+def read_config(section):
+    """读取指定部分的配置项"""
+    return {k: v for k, v in config.items(section)}
+
+def create_form(title, section, option_v):
+    """创建表单，用于用户输入参数"""
+    num_items = read_config(section)
+    
+    with st.form("setting"):
+        tab1, tab2 = st.tabs(["运算项", "布局"])  # 创建两个标签页
+        
+        with tab1:
+            totalNum = st.slider('生成算式数量', min_value=6, max_value=100, value=int(num_items['ss_number']), step=2)  # 生成算式数量滑动条
+            
+            jiaBool = st.checkbox('加法', value=True)  # 加法复选框
+            jiaMin, jiaMax = create_number_inputs('加法', 'jia', num_items)  # 加法最小值和最大值输入框
+            
+            jianBool = st.checkbox('减法', value=True)  # 减法复选框
+            jianMin, jianMax = create_number_inputs('减法', 'jian', num_items)  # 减法最小值和最大值输入框
+            
+            chengBool = st.checkbox('乘法')  # 乘法复选框
+            chengMin, chengMax = create_number_inputs('乘法', 'cheng', num_items)  # 乘法最小值和最大值输入框
+            
+            chuBool = st.checkbox('除法')  # 除法复选框
+            deshu_max = st.slider('得数限制', min_value=1, max_value=99, value=int(num_items['deshu_max']), step=1)  # 得数限制滑动条
+            beichuMin, beichuMax = create_number_inputs('被除数', 'beichu', num_items)  # 被除数最小值和最大值输入框
+            chuMin, chuMax = create_number_inputs('除数', 'chu', num_items)  # 除数最小值和最大值输入框
+            
+            yuShuList = st.selectbox('是否生成带余数算式？', ('不要生成带余数算式', '总是生成带余数算式', '随机产生带余数算式'), index=1)  # 带余数算式选择框
+            yuShu = {'不要生成带余数算式': 0, '总是生成带余数算式': 1, '随机产生带余数算式': 2}[yuShuList]  # 将选择转换为数字
+        
+        with tab2:
+            t_c_spacing = st.slider('间距', min_value=50, max_value=150, value=int(num_items['c_spacing']), step=5)  # 间距滑动条
+            t_r_spacing = st.slider('行距', min_value=5, max_value=150, value=int(num_items['r_spacing']), step=5)  # 行距滑动条
+            t_g_num = st.slider('每行算式数量', min_value=2, max_value=5, value=int(num_items['s_num']), step=1)  # 每行算式数量滑动条
+        
+        create = st.form_submit_button('生成算式', type="primary", use_container_width=True)  # 提交按钮
+        
+        return {
+            'totalNum': totalNum,
+            'jiaBool': jiaBool,
+            'jiaMin': jiaMin,
+            'jiaMax': jiaMax,
+            'jianBool': jianBool,
+            'jianMin': jianMin,
+            'jianMax': jianMax,
+            'chengBool': chengBool,
+            'chengMin': chengMin,
+            'chengMax': chengMax,
+            'chuBool': chuBool,
+            'deshu_max': deshu_max,
+            'beichuMin': beichuMin,
+            'beichuMax': beichuMax,
+            'chuMin': chuMin,
+            'chuMax': chuMax,
+            'yuShu': yuShu,
+            't_c_spacing': t_c_spacing,
+            't_r_spacing': t_r_spacing,
+            't_g_num': t_g_num,
+            'create': create
+        }
+
+def create_number_inputs(label, prefix, num_items):
+    """创建数字输入框"""
+    col1, col2 = st.columns(2)
+    with col1:
+        min_val = st.number_input(f'{label}最小取值', value=int(num_items[f'{prefix}_min']), min_value=DEFAULT_MIN_VALUE, step=1, format='%d')  # 最小值输入框
+    with col2:
+        max_val = st.number_input(f'{label}最大取值', value=int(num_items[f'{prefix}_max']), min_value=DEFAULT_MIN_VALUE, step=1, format='%d')  # 最大值输入框
+    return min_val, max_val
+
+def generate_html(suanshi, t_c_spacing, t_r_spacing, t_g_num, title):
+    """生成 HTML 代码"""
+    td = "<td style='vertical-align:top'>"
+    td_end = "</td>"
+    html_code = ""
+    
+    e_num = len(suanshi)
+    i = 1
+    
+    while i <= e_num:
+        s = ""
+        ss_row = math.ceil(e_num / t_g_num)
+        for n in range(ss_row):
+            if i <= e_num:
+                s += f"<table align='center' style='border-collapse:separate;border-spacing:{t_c_spacing}px 0px;margin-bottom:{t_r_spacing}px'>{suanshi[i-1]}</table>"
+            if n == ss_row - 1:
+                s = td + s + td_end
+            i += 1
+        html_code += s
+    
+    html_start = f"<p align='center'><font size='5'>{title}练习题</font></p><p align='center'><font size='4'>_______年___月___日&nbsp&nbsp&nbsp&nbsp姓名:__________&nbsp&nbsp&nbsp&nbsp用时:_________</font></p><table align='center' border='0'>"
+    html_end = "</table>"
+    return html_start + html_code + html_end
+
+@st.cache_data
+def get_version():
+    """获取最新版本信息"""
+    url = "https://api.github.com/repos/gc313/kousuan/releases/latest"
+    try:
+        res = requests.get(url, verify=False)
+        res.raise_for_status()
+        return res.json()["tag_name"]
+    except requests.exceptions.RequestException as e:
+        st.error(f"请求失败: {e}")
+    except KeyError:
+        st.error("无法解析版本信息")
+    except Exception as e:
+        st.error(f"未知错误: {e}")
+    return "Unknown"
 
 with st.sidebar:
     st.subheader("🏮练习题生成参数设置🏮")
     
-    #算式种类选择
-    option = st.selectbox("选择算式种类", ('计时口算','竖式计算'))
-    option_dict = {'计时口算':0, '竖式计算':1}
+    option = st.selectbox("选择算式种类", ('计时口算', '竖式计算'))  # 算式种类选择框
+    option_dict = {'计时口算': 0, '竖式计算': 1}
     option_v = option_dict[option]
-
-    #读取配置文件
-    file = 'setting.ini'
-    config = configparser.ConfigParser()
-    config.read(file, encoding = 'utf-8')
-
-    with st.form("setting"):
-        tab1, tab2 = st.tabs(["运算项","布局"])
-        
-        #计时口算UI
-        if option_v == 0:
-            num_items = dict(config.items('KOUSUAN'))
-            title = "计时口算"
-            
-            with tab1:
-
-                totalNum = st.slider('生成算式数量', min_value=6, max_value=100, value=int(num_items['ss_number']), step=2, format=None)
-                
-                jiaBool = st.checkbox('加法', value=True)
-                jia_col1, jia_col2 = st.columns(2)
-                with jia_col1:
-                    jiaMin = st.number_input('运算项最小取值', value= int(num_items['jia_min']), min_value=0, step=1, format='%d',key=1)
-                with jia_col2:
-                    jiaMax = st.number_input('运算项最大取值', value= int(num_items['jia_max']), min_value=0, step=1, format='%d',key=2)
-
-                jianBool = st.checkbox('减法', value=True)
-                jian_col1, jian_col2 = st.columns(2)
-                with jian_col1:
-                    jianMin = st.number_input('运算项最小取值', value= int(num_items['jian_min']), min_value=0, step=1, format='%d',key=3)
-                with jian_col2:
-                    jianMax = st.number_input('运算项最大取值', value= int(num_items['jian_max']), min_value=0, step=1, format='%d',key=4)
-
-                chengBool = st.checkbox('乘法')
-                cheng_col1, cheng_col2 = st.columns(2)
-                with cheng_col1:
-                    chengMin = st.number_input('运算项最小取值', value= int(num_items['cheng_min']), min_value=0, step=1, format='%d',key=5)
-                with cheng_col2:
-                    chengMax = st.number_input('运算项最大取值', value= int(num_items['cheng_max']), min_value=0, step=1, format='%d',key=6)
-
-                chuBool = st.checkbox('除法')
-                #设置一个得数限制，数太大了小朋友做不了
-                deshu_max = st.slider('得数限制', min_value=1, max_value=99, value=int(num_items['deshu_max']), step=1, format=None)
-                chu_col1, chu_col2 = st.columns(2)
-                with chu_col1:
-                    beichuMin = st.number_input('被除数最小取值', value= int(num_items['beichu_min']), min_value=0, step=1, format='%d',key=7)
-                with chu_col2:
-                    beichuMax = st.number_input('被除数最大取值', value= int(num_items['beichu_max']), min_value=0, step=1, format='%d',key=8)
-
-                chu_col3, chu_col4 = st.columns(2)
-                with chu_col3:
-                    chuMin = st.number_input('除数最小取值', value= int(num_items['chu_min']), min_value=1, step=1, format='%d')
-                with chu_col4:
-                    chuMax = st.number_input('除数最大取值', value= int(num_items['chu_max']), min_value=1, step=1, format='%d')
-
-                yuShuList = st.selectbox('是否生成带余数算式？', ('不要生成带余数算式', '总是生成带余数算式', '随机产生带余数算式' ),index = 1)
-                yuShudict = {'不要生成带余数算式':0, '总是生成带余数算式':1, '随机产生带余数算式':2,}
-                yuShu = yuShudict[yuShuList]  
-        
-        
-        #竖式计算UI
-        if option_v == 1:
-            num_items = dict(config.items('SHUSHI'))
-            title = "竖式计算"
-            
-            with tab1:        
-                totalNum = st.slider('生成算式数量', min_value=6, max_value=100, value=int(num_items['ss_number']), step=2, format=None)
-                
-                jiaBool = st.checkbox('加法', value=True)
-                jia_col1, jia_col2 = st.columns(2)
-                with jia_col1:
-                    jiaMin = st.number_input('运算项最小取值', value= int(num_items['jia_min']), min_value=0, step=1, format='%d',key=1)
-                with jia_col2:
-                    jiaMax = st.number_input('运算项最大取值', value= int(num_items['jia_max']), min_value=0, step=1, format='%d',key=2)
-
-                jianBool = st.checkbox('减法', value=True)
-                jian_col1, jian_col2 = st.columns(2)
-                with jian_col1:
-                    jianMin = st.number_input('运算项最小取值', value= int(num_items['jian_min']), min_value=0, step=1, format='%d',key=3)
-                with jian_col2:
-                    jianMax = st.number_input('运算项最大取值', value= int(num_items['jian_max']), min_value=0, step=1, format='%d',key=4)
-
-                chengBool = st.checkbox('乘法')
-                cheng_col1, cheng_col2 = st.columns(2)
-                with cheng_col1:
-                    chengMin = st.number_input('运算项最小取值', value= int(num_items['cheng_min']), min_value=0, step=1, format='%d',key=5)
-                with cheng_col2:
-                    chengMax = st.number_input('运算项最大取值', value= int(num_items['cheng_max']), min_value=0, step=1, format='%d',key=6)
-
-                chuBool = st.checkbox('除法')
-                #设置一个得数限制，数太大了小朋友做不了
-                deshu_max = st.slider('得数限制', min_value=1, max_value=99, value=int(num_items['deshu_max']), step=1, format=None)
-                chu_col1, chu_col2 = st.columns(2)
-                with chu_col1:
-                    beichuMin = st.number_input('被除数最小取值', value= int(num_items['beichu_min']), min_value=0, step=1, format='%d',key=7)
-                with chu_col2:
-                    beichuMax = st.number_input('被除数最大取值', value= int(num_items['beichu_max']), min_value=0, step=1, format='%d',key=8)
-
-                chu_col3, chu_col4 = st.columns(2)
-                with chu_col3:
-                    chuMin = st.number_input('除数最小取值', value= int(num_items['chu_min']), min_value=1, step=1, format='%d')
-                with chu_col4:
-                    chuMax = st.number_input('除数最大取值', value= int(num_items['chu_max']), min_value=1, step=1, format='%d')
-
-                yuShuList = st.selectbox('是否生成带余数算式？', ('不要生成带余数算式', '总是生成带余数算式', '随机产生带余数算式' ),index = 1)
-                yuShudict = {'不要生成带余数算式':0, '总是生成带余数算式':1, '随机产生带余数算式':2,}
-                yuShu = yuShudict[yuShuList]
-        
-        
-        with tab2:
-            #行间距设置
-            t_c_spacing = st.slider('间距', min_value=50, max_value=150, value=int(num_items['c_spacing']), step=5, format=None)
-            t_r_spacing = st.slider('行距', min_value=5, max_value=150, value=int(num_items['r_spacing']), step=5, format=None)
-            t_g_num = st.slider('每行算式数量', min_value=2, max_value=5, value=int(num_items['s_num']), step=1, format=None)
-
-        create = st.form_submit_button('生成算式', type="primary", use_container_width = True)
-        #在线获取版本
-        @st.cache_data    
-        def GetVersion():
-            res = requests.get("https://api.github.com/repos/gc313/kousuan/releases/latest")
-            try:
-                output = res.json()["tag_name"]
-            except:
-                output = "Unknow"
-            return output
-
-
-        def Html(suanshi):
-            #构建html语句，这部分是要打印出的算式内容
-            """思路是先生成一个N列1行的表，每一列里面由上至下生成算式，每一个算式放
-            在单独的一个表里。
-            原因：之前用border-spacing控制表格间距时，上下左右都会同时变化变化，但
-            我只想变化水平间距和下间距。所以想到把每一行做成单独的表格，用border-spacing
-            的前一个参数控制水平间距，用margin-bottom控制下间距，但这有个问题，生成的算式
-            长短不一，所以每行形成的表格难以对齐，用现在的方法，每一个包含算式的小表格被放
-            在一个大表格的列里，默认就左对齐了。
-            """
-            td = "<td style='vertical-align:top'>"
-            td_end = "</td>"
-            html_code = ""
-                
-            #获取列表中算式个数
-            e_num = len(suanshi)
-            i = 1
-            #循环至获取所有算式
-            while i <= e_num:
-                s = ""
-                #根据算式总数除以设定的列数，得到需要生成多少行算式，除不尽的用math.ceil()向上取整
-                ss_row = math.ceil(e_num / t_g_num)
-                for n in range(ss_row):
-                    #再次判断是否获取所有元素?这里可以优化？？
-                    if i <= e_num:
-                        #每列里的每个算式单独生成一个表格
-                        s += f"<table align='center' style= 'border-collapse:separate;border-spacing:{t_c_spacing}px 0px;margin-bottom:{t_r_spacing}px'>"+suanshi[i-1]+"</table>"
-                    #当生成每列所需数量的算式后，加上html的<td>标签，形成一列
-                    if n == ss_row - 1:
-                        s = td + s + td_end
-                    i += 1
-                #把生成的一列算式html代码存入html_code
-                html_code += s
-
-            return html_code
-
-        #判断生成算式按钮是否被按下，并把Html语句放入变量
-        html_start = f"<p align='center'><font size='5'>{title}练习题</font></p><p align='center'><font size='4'>_______年___月___日&nbsp&nbsp&nbsp&nbsp姓名:__________&nbsp&nbsp&nbsp&nbsp用时:_________</font></p><table align='center' border='0'>"
-        html_end = "</table>"
-        #没创建算式时只显示标题
-        fin_suanshi = html_start + html_end
     
-    #版本信息
-    st.caption(GetVersion())
-
-
+    if option_v == 0:
+        form_data = create_form("计时口算", 'KOUSUAN', option_v)  # 创建计时口算表单
+    elif option_v == 1:
+        form_data = create_form("竖式计算", 'SHUSHI', option_v)  # 创建竖式计算表单
+    # 版本信息
+    st.caption(get_version())
 
 with st.container():
-
-    #点击按钮
-    if create:
-        type_list = [jiaBool, jianBool, chengBool, chuBool]
+    fin_suanshi = ""  # 初始化 fin_suanshi 变量
+    if form_data['create']:
+        type_list = [form_data['jiaBool'], form_data['jianBool'], form_data['chengBool'], form_data['chuBool']]
         true_num = type_list.count(True)
-        if true_num >0:
-            nr = bd.CreateSS(totalNum, jiaBool, jiaMin, jiaMax, jianBool, jianMin, jianMax, chengBool, chengMin, chengMax, chuBool, beichuMin, beichuMax, chuMin, chuMax, yuShu, deshu_max, true_num)
-            fin_suanshi = html_start + Html(nr) + html_end
+        if true_num > 0:
+            nr = bd.create_ss(
+                form_data['totalNum'],
+                form_data['jiaBool'], form_data['jiaMin'], form_data['jiaMax'],
+                form_data['jianBool'], form_data['jianMin'], form_data['jianMax'],
+                form_data['chengBool'], form_data['chengMin'], form_data['chengMax'],
+                form_data['chuBool'], form_data['beichuMin'], form_data['beichuMax'],
+                form_data['chuMin'], form_data['chuMax'], form_data['yuShu'],
+                form_data['deshu_max'], true_num
+            )  # 调用 create_ss 函数生成算式
+            fin_suanshi = generate_html(nr, form_data['t_c_spacing'], form_data['t_r_spacing'], form_data['t_g_num'], option)  # 生成 HTML 代码
         else:
             st.warning("没有选择任何算式种类，不会生成任何算式！", icon="🚨")
 
-    #调出打印机的javascript代码
     print_js = """
     function printer(){
     var p_dlg = window.open('printer', '', '');
@@ -230,11 +186,8 @@ with st.container():
     }
     """
 
-    #把其他代码组合进来
-    #要打印的页面内容不和其他代码放在一起就找不到！
     my_html = f"""<script>{print_js}</script>
     <button onclick="printer()">打印算式</button>
     <div id='here'>{fin_suanshi}</div>
     """
-    components.html(my_html, height = 1080, scrolling=True)
-    
+    st.components.v1.html(my_html, height=1080, scrolling=True)  # 显示生成的 HTML 代码
